7ba9cff00109a5d0fc3034a704e6d8313c9541aa
koechronix <koechronix@yahoo.com>
2022-07-13 10:39:46 -0400

    commit tree 0f53ec5b6e380a8a416055b2d0cc1108ea8eb3e1

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,6 @@
+import userEvent from "@testing-library/user-event";
 import React from "react";
+import {image} from "../data/user"
 
 function About() {
   return (
@@ -9,3 +11,5 @@ function About() {
     </div>
   );
 }
+
+export default About;
